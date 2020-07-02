e50839bd6d9f1c2b4129864402d1b1b90963a924
Alessio Roberto <darthpelo@gmail.com>
2020-07-02 22:04:25 +0200

    Project initialization

---
+++ SixtyFour/SixtyFour/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SixtyFour
+//
+//  Created by Alessio Roberto on 02/07/2020.
+//  Copyright © 2020 Alessio Roberto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
